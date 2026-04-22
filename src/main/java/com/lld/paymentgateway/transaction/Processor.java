05091810c1161dfb06305e18c983aa79c2ceac8d
pratham.agarwal <t-pratham.agarwal@ocltp.com>
2026-04-22 15:18:36 +0530

    added payment gateway

---
+++ src/main/java/com/lld/paymentgateway/transaction/Processor.java
@@ -0,0 +1,20 @@
+package com.lld.paymentgateway.transaction; 
+
+import com.lld.paymentgateway.instrument.InstrumentDO;
+
+public class Processor {
+
+    public void processPayment(InstrumentDO senderInstrumentDO, InstrumentDO receiverInstrumentDO) {
+
+        // validate sender instrument
+
+        // validate receiver instrument
+
+        // process payment
+
+        // update balance in sender instrument - debit
+
+        // update balance in receiver instrument - credit
+
+    }
+}
